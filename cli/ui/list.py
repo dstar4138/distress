0c1251416cba665e6a270b4748ec414cd3460701
Alexander Dean <dstar@csh.rit.edu>
2014-03-23 04:23:17 -0400

    Adds list command. Since it was simple to implement, I decided to give it a shot.

--- cli/ui/list.py
+++ cli/ui/list.py
@@ -1,16 +1,42 @@
 """
-usage: distress list
+Returns list of all local reciepts
 
-returns list of all local reciepts
+Usage: 
+    distress list [options]
 
+Options:
+    -p --no-pretty-print    Toggle pretty print off, useful for piping
+    -v --verbose    Print as much information as possible
 """
-from docopt import docopt
+from _config import build_cmd_args
 
+def printmeta( ida, meta, pp ):
+    ID,FN,E,DEL,FIN='ID: ','\t Filename: ','\n',': ','------\n' 
+    if not pp: ID,FN,E,DEL,FIN='','\t','','\t','\n'
 
+    print ID,ida,FN,meta['name'],E,
+    for x,y in meta.items():
+        if x == 'name': continue
+        if pp: print x,
+        print DEL,y,E,
+    print FIN,      
+
+def list_( socket, args, config, library, cmd=False ):
+    """ Handles user input to list all files the client stored in the
+        DISTRESS network. It does so by checking the library only.
+    """
+    verbose = args['--verbose']
+    pp = not args['--no-pretty-print']
+
+    if not verbose:
+        names = library.get_names()
+        for fname in names: print fname
+    else:
+        info = library.meta()
+        for ida,meta in info.items():
+            printmeta( ida, meta, pp )
+            
 if __name__ == '__main__':
-	args = docopt (__doc__,
-                   options_first=False)
-
-	print args
-
+    (socket, args, config, library) = build_cmd_args( __doc__ ) 
+    list_( socket, args, config, library, cmd=True )
 
